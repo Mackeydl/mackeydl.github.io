5ecd43c1ccae9cba18d011c4befe0ce666503a06
Mackeydl <macaire.denby@education.nsw.gov.au>
2016-11-07 20:53:50 +1100

    v3.3

---
+++ js/random-phrase.js
@@ -0,0 +1,16 @@
+var request = new XMLHttpRequest();
+request.onload = function() {
+    // get the file contents
+    var fileContent = this.responseText;
+    // split into lines
+    var fileContentLines = fileContent.split( '\n' );
+    // get a random index (line number)
+    var randomLineIndex = Math.floor( Math.random() * fileContentLines.length );
+    // extract the value
+    var randomLine = fileContentLines[ randomLineIndex ];
+
+    // add the random line in a div
+    document.getElementById( 'random-phrase' ).innerHTML = randomLine;
+};
+request.open( 'GET', 'http://mackeydl.github.io/media/text.txt', true );
+request.send();
